e3b12a925ac1e4ca073cb468040ad1d36d5d73ac
Kauan Farias <kauandefarias@yahoo.com.br>
2025-11-04 17:21:31 -0300

    🐛 fix: point to the right CSS file

--- adicionar_pet.php
+++ adicionar_pet.php
@@ -24,7 +24,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>UBC: Adicionar pet</title>
-    <link rel="stylesheet" href="styles/cadastro.css">
+    <link rel="stylesheet" href="styles/common.css">
     <style>
         .bloco-form {
             width: 100%;
